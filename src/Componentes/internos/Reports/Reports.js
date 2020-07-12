5173f5fc1b85e54d03a5aade1789a8cf9cc50f8d
virus77 <bus732@gmail.com>
2020-07-12 08:12:56 -0500

    commit

--- src/Componentes/internos/Reports/Reports.js
+++ src/Componentes/internos/Reports/Reports.js
@@ -5,7 +5,6 @@ import { Button, Card, Form } from 'react-bootstrap';
 import DatePicker from "react-datepicker";
 import { registerLocale } from "react-datepicker";
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
