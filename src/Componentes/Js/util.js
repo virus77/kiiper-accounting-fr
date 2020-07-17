4342cbc989b3f6149d73bd22ad6f33f836d5ec8f
virus77 <bus732@gmail.com>
2020-07-16 19:53:46 -0500

    commit

--- src/Componentes/Js/util.js
+++ src/Componentes/Js/util.js
@@ -865,7 +865,7 @@ Datepicker.prototype.init = function (params) {
         dayNamesMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"],
         dayNamesShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
         monthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
-        monthNamesShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dec"]
+        monthNamesShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"]
     });
 };
 
